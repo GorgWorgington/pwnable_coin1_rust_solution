7fa9988019381a3c469e20f9c44a73cc0ace22c3
Gorg Worgington <gorgworg@gmail.com>
2021-12-01 13:09:59 -0800

    Added header comment

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,44 @@
+//Unlike my C solution, this one has DNS resolution.
+//`cargo run www.pwnable.kr 9007`
+
+/*
+	---------------------------------------------------
+	-              Shall we play a game?              -
+	---------------------------------------------------
+	
+	You have given some gold coins in your hand
+	however, there is one counterfeit coin among them
+	counterfeit coin looks exactly same as real coin
+	however, its weight is different from real one
+	real coin weighs 10, counterfeit coin weighes 9
+	help me to find the counterfeit coin with a scale
+	if you find 100 counterfeit coins, you will get reward :)
+	FYI, you have 60 seconds.
+	
+	- How to play - 
+	1. you get a number of coins (N) and number of chances (C)
+	2. then you specify a set of index numbers of coins to be weighed
+	3. you get the weight information
+	4. 2~3 repeats C time, then you give the answer
+	
+	- Example -
+	[Server] N=4 C=2 	# find counterfeit among 4 coins with 2 trial
+	[Client] 0 1 		# weigh first and second coin
+	[Server] 20			# scale result : 20
+	[Client] 3			# weigh fourth coin
+	[Server] 10			# scale result : 10
+	[Client] 2 			# counterfeit coin is third!
+	[Server] Correct!
+
+*/
 
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::net::ToSocketAddrs;
 use std::io::Read;
 use std::str;
-use std::error::Error;
 use std::env;
+use std::fmt;
 
 enum CheckState {
     CounterfeitInside,
@@ -15,12 +48,21 @@ enum CheckState {
 
 const READ_BYTES: usize = 2048;
 
+#[derive(Debug, Clone)]
+struct FindNumError;
+
+impl fmt::Display for FindNumError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		write!(f, "number was not found")
+	}
+}
+
 trait FindNum {
-    fn find_num_slice(&self, start: usize) -> (&[u8], usize); //finds a slice containing a number
+    fn find_num_slice(&self, start: usize) -> Result<(&[u8], usize), FindNumError>; //finds a slice containing a number
 }
 
 impl FindNum for [u8] {
-    fn find_num_slice(&self, start: usize) -> (&[u8], usize) {
+    fn find_num_slice(&self, start: usize) -> Result<(&[u8], usize), FindNumError> {
         //NOTE: currently does not handle the possibility of a number not being found.
         const NUMS_STR: &[u8] = b"1234567890";
         let mut idx1: usize = start;
@@ -47,43 +89,54 @@ impl FindNum for [u8] {
             idx2 += 1;
         }
         //println!("{:?}", &self[idx1..idx2]);
-        return (&self[idx1..idx2], idx2);
+        return Ok((&self[idx1..idx2], idx2));
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> () {
     
     let args: Vec<String> = env::args().collect();
 
     //let serv_addr: String = args[1].clone();
     let serv_addr = &*args[1];
-    let serv_port: u16 = args[2].parse()?;
+    let serv_port: u16 = args[2].parse().expect("ERROR:invalid port");
 
     let mut socket = TcpStream::connect(
         (serv_addr, serv_port)
-        .to_socket_addrs()? //This is where the address is resolved
+        .to_socket_addrs().unwrap() //This is where the address is resolved
         .next()
-        .expect("ERROR:Address could not be resolved.")
-    )?;
+        .expect("ERROR:address could not be resolved")
+    ).expect("ERROR:could not esablish connection");
 
-    let mut read_buf: [u8; READ_BYTES] = [0; READ_BYTES];
-    let mut num_bytes: usize;
+	let mut read_buf: [u8; READ_BYTES] = [0; READ_BYTES];
+	let mut num_bytes: usize;
     
-    socket.read(&mut read_buf)?; //initial mesg
-
+    socket.read(&mut read_buf).unwrap(); //initial mesg
+	
     loop {
         println!("DEBUG:starting outer loop");
-        num_bytes = socket.read(&mut read_buf)?;
+
+		loop{
+			let read_result = socket.read(&mut read_buf);
+			match read_result {
+				Ok(n) => num_bytes = n,
+				Err(e) => match e.kind(){
+					std::io::ErrorKind::Interrupted => continue, //Retry the read()
+					_ => panic!("ERROR:failed to read packet:{}", e),
+				}
+			}
+			break; //This loop is kind of gross. I wonder if there is a more elegant way of doing this.
+		}
         if num_bytes == 0 {
             break;
         }
         let read_mesg = &read_buf[0..num_bytes]; //slice of the buffer containing what was just read.
-        print!("SERVER:{}", str::from_utf8(read_mesg)?);
+        print!("SERVER:{}", str::from_utf8(read_mesg).unwrap());
 
-        let (n_slice, _) = read_mesg.find_num_slice(0);
+        let (n_slice, _) = read_mesg.find_num_slice(0).expect("ERROR:couldn't find number in buffer");
         //let (c_slice, _) = read_mesg.find_num_slice(end);
 
-        let n: i64 = str::from_utf8(n_slice)?.parse::<i64>()?;
+        let n: i64 = str::from_utf8(n_slice).unwrap().parse::<i64>().unwrap(); //Turbofish is redundant here because n has an explicit type.
         println!("DEBUG:n = {}", n);
         //let c: i64 = str::from_utf8(c_slice)?.parse::<i64>()?; //Doesn't get used...
 
@@ -107,7 +160,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         range_mid = (range_end + range_beg) / 2;
         }
     }
-    Ok(())
 }
 
 fn check(range_beg: i64, range_end: i64, read_buf: &mut [u8], socket: &mut TcpStream) -> CheckState {
@@ -130,7 +182,7 @@ fn check(range_beg: i64, range_end: i64, read_buf: &mut [u8], socket: &mut TcpSt
         return CheckState::CounterfeitFound
     }
     if read_mesg[0] == 't' as u8 {
-        panic!("DEBUG:Failed; time expired");
+        panic!("DEBUG:failed; time expired");
     }
 
     let weight = str::from_utf8(read_mesg).unwrap().parse::<i64>().unwrap();
